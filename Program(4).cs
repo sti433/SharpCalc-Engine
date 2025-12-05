2035d07e25b5f07a5e8ecf06a44bf071f6822410
sti433 <sti.ri433@gmail.com>
2025-12-05 20:50:18 +0330

    Add files via upload

---
+++ Program(4).cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, sum, s;
+            s = 0;
+            sum = 0;
+            a = float.Parse(Console.ReadLine());
+            float max, min;
+            max = a;
+            min = a;
+            sum = sum + a;
+            for (int i = 1; i <= 14; i++)
+            {
+                s = float.Parse(Console.ReadLine());
+                if (s < min)
+                    min = s;
+                if (s > max)
+                    max = s;
+                sum += s;
+                continue;
+            }
+            Console.WriteLine("min" + min);
+            Console.WriteLine("max" + max);
+            float e = max - min;
+            Console.WriteLine("e" + e);
+            Console.WriteLine("sum" + sum);
+            float avg = sum / 15;
+            Console.WriteLine("avg" + avg);
+
+            Console.ReadKey();
+        }
+    }
+}
